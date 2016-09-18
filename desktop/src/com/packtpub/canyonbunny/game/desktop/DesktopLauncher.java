331eaa55b7d19e7d3b260fa7cc9db14ca3ff4b9c
Dbecker16 <dbecker24@gmail.com>
2016-09-18 16:01:39 -0400

    Added rest of game files
    Added the rest of canyon bunny game files

---
+++ desktop/src/com/packtpub/canyonbunny/game/desktop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.packtpub.canyonbunny.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.packtpub.canyonbunny.game.CanyonBunnyMain;
+
+public class DesktopLauncher
+{
+	public static void main (String[] arg)
+	{
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CanyonBunnyMain(), config);
+		config.width = 800;
+		config.height = 400;
+	}
+}
